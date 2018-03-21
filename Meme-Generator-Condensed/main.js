32981d024a1beca4b68753a297d77353f5389e05
Tomer Preis <preis.tomer@gmail.com>
2018-03-21 18:48:26 +0200

    Added many features to manipulate text in meme generator

--- Meme-Generator-Condensed/main.js
+++ Meme-Generator-Condensed/main.js
@@ -1,10 +1,5 @@
 'use strict';
 
-
-var inputBottom = document.getElementById('bottom').value;
-var inputTop = document.getElementById('top').value;
-
-
 var gImgs = [
     {
         id: 1,
@@ -15,16 +10,24 @@ var gImgs = [
 var gMeme = {
     selectedImgId: 1, txts: [
         {
-            line: inputTop,
-            size: 20,
-            align: 'center',
-            color: 'red'
+            line: '',
+            font: 'Impact',
+            size: 50,
+            align: 'right',
+            color: '#ffffff',
+            shadow: 'none',
+            posX: 250,
+            posY: 70
         },
         {
-            line: inputBottom,
-            size: 20,
-            align: 'center',
-            color: 'red'
+            line: '',
+            font: 'Impact',
+            size: 50,
+            align: 'right',
+            color: '#ffffff',
+            shadow: 'none',
+            posX: 250,
+            posY: 363
         }
     ]
 }
@@ -33,23 +36,27 @@ var gMeme = {
 function drawImage() {
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
-    
+
     var img = new Image();
     img.src = gImgs[0].url;
-    
+
     img.onload = function () {
         context.drawImage(img, 0, 0, 500, 400);
-        context.font = "40px 'Segoe UI'";
-        context.fillStyle = 'white';
-        context.strokeStyle = 'black';
-        context.lineWidth = 1.5; 
-        context.strokeText(gMeme.txts[0].line, 40, 70);
-        context.fillText(gMeme.txts[0].line, 40, 70);
-        context.strokeText(gMeme.txts[1].line, 40, 350);
-        context.fillText(gMeme.txts[1].line, 40, 350);
+
+        gMeme.txts.forEach(function (txt, i) {
+
+            context.font = ` ${txt.size}px ${txt.font}`;
+            context.fillStyle = txt.color;
+            context.shadowColor = txt.shadow;
+            context.shadowBlur = 7;
+            context.shadowOffsetX = 5;
+            context.shadowOffsetY = 5;
+            context.textAlign = txt.align;
+            context.strokeText(txt.line, txt.posX, txt.posY);
+            context.fillText(txt.line, txt.posX, txt.posY);
+        })
     };
 }
-
 
 
 function drawText() {
@@ -58,18 +65,66 @@ function drawText() {
     var inputBottom = document.getElementById('bottom').value;
     gMeme.txts[1].line = inputBottom;
     drawImage();
-    
-    
-    
 }
 
-function alignText(){
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    context.textAlign= gMeme.txts[0].align; 
-
+function alignText() {
+    var inputAlignTop = document.getElementById('align-top').value
+    var inputAlignBottom = document.getElementById('align-bottom').value
+    gMeme.txts[0].align = inputAlignTop;
+    gMeme.txts[1].align = inputAlignBottom;
+    drawImage();
 }
 
+function changeFontSize() {
+    var inputSizeTop = document.getElementById('mm-size-top').value;
+    var inputSizeBottom = document.getElementById('mm-size-bottom').value;
+    gMeme.txts[0].size = inputSizeTop;
+    gMeme.txts[1].size = inputSizeBottom;
+    drawImage();
+}
+
+function changeColor() {
+    var colorTop = document.getElementById("color-top").value;
+    var colorBottom = document.getElementById("color-bottom").value;
+    gMeme.txts[0].color = colorTop;
+    gMeme.txts[1].color = colorBottom;
+    drawImage();
+}
+
+
+function addShadow() {
+    var isTopChecked = document.getElementById("switch-top").checked;
+    var isBottomChecked = document.getElementById("switch-bottom").checked;
+
+    if (isTopChecked) {
+        gMeme.txts[0].shadow = 'black';
+    } else {
+        gMeme.txts[0].shadow = 'rgba(0, 0, 0, 0)';
+    };
+   drawImage();
+
+    if (isBottomChecked) {
+        gMeme.txts[1].shadow = 'black';
+    } else {
+        gMeme.txts[1].shadow = 'rgba(0, 0, 0, 0)';
+    };
+    drawImage();
+}
+
+
+function changeFont(){
+    var fontTop = document.getElementById("font-top").value;
+    var fontBottom = document.getElementById("font-bottom").value;
+    gMeme.txts[0].font = fontTop;
+    gMeme.txts[1].font = fontBottom;
+    drawImage();
+}
+
+function move(idx, jumpHeight){
+    gMeme.txts[idx].posY += jumpHeight;
+   
+    drawImage();
+}
 
 
 function clearCanvas() {
@@ -82,8 +137,8 @@ function clearCanvas() {
     ctx.restore();        // restore the transform
     var img = new Image();
     img.src = gImgs[0].url;
-    
+
     img.onload = function () {
-    ctx.drawImage(img, 0, 0, 500, 400);
-}
+        ctx.drawImage(img, 0, 0, 500, 400);
+    }
 }
